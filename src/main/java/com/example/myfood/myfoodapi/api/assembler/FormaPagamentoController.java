0ed7c6d9438c6e277e6023eab610823231dd419d
Pedro Miranda <pcamposmiranda@gmail.com>
2022-02-28 15:13:34 -0300

    crud forma de pagamento

---
+++ src/main/java/com/example/myfood/myfoodapi/api/assembler/FormaPagamentoController.java
@@ -0,0 +1,83 @@
+package com.example.myfood.myfoodapi.api.assembler;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.example.myfood.myfoodapi.api.model.FormaPagamentoModel;
+import com.example.myfood.myfoodapi.api.model.input.FormaPagamentoInput;
+import com.example.myfood.myfoodapi.domain.model.FormaPagamento;
+import com.example.myfood.myfoodapi.domain.repository.FormaPagamentoRepository;
+import com.example.myfood.myfoodapi.domain.service.CadastroFormaPagamentoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/formas-pagamento")
+public class FormaPagamentoController {
+
+	@Autowired
+	private FormaPagamentoRepository formaPagamentoRepository;
+	
+	@Autowired
+	private CadastroFormaPagamentoService cadastroFormaPagamento;
+	
+	@Autowired
+	private FormaPagamentoModelAssembler formaPagamentoModelAssembler;
+	
+	@Autowired
+	private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler;
+	
+	@GetMapping
+	public List<FormaPagamentoModel> listar() {
+		List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
+		
+		return formaPagamentoModelAssembler.toCollectionModel(todasFormasPagamentos);
+	}
+	
+	@GetMapping("/{formaPagamentoId}")
+	public FormaPagamentoModel buscar(@PathVariable Long formaPagamentoId) {
+		FormaPagamento formaPagamento = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
+		
+		return formaPagamentoModelAssembler.toModel(formaPagamento);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public FormaPagamentoModel adicionar(@RequestBody @Valid FormaPagamentoInput formaPagamentoInput) {
+		FormaPagamento formaPagamento = formaPagamentoInputDisassembler.toDomainObject(formaPagamentoInput);
+		
+		formaPagamento = cadastroFormaPagamento.salvar(formaPagamento);
+		
+		return formaPagamentoModelAssembler.toModel(formaPagamento);
+	}
+	
+	@PutMapping("/{formaPagamentoId}")
+	public FormaPagamentoModel atualizar(@PathVariable Long formaPagamentoId,
+			@RequestBody @Valid FormaPagamentoInput formaPagamentoInput) {
+		FormaPagamento formaPagamentoAtual = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
+		
+		formaPagamentoInputDisassembler.copyToDomainObject(formaPagamentoInput, formaPagamentoAtual);
+		
+		formaPagamentoAtual = cadastroFormaPagamento.salvar(formaPagamentoAtual);
+		
+		return formaPagamentoModelAssembler.toModel(formaPagamentoAtual);
+	}
+	
+	@DeleteMapping("/{formaPagamentoId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long formaPagamentoId) {
+		cadastroFormaPagamento.excluir(formaPagamentoId);	
+	}
+	
+}
---
+++ src/main/java/com/example/myfood/myfoodapi/api/assembler/FormaPagamentoModelAssembler.java
@@ -0,0 +1,29 @@
+package com.example.myfood.myfoodapi.api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.myfood.myfoodapi.api.model.FormaPagamentoModel;
+import com.example.myfood.myfoodapi.domain.model.FormaPagamento;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FormaPagamentoModelAssembler {
+
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public FormaPagamentoModel toModel(FormaPagamento formaPagamento) {
+		return modelMapper.map(formaPagamento, FormaPagamentoModel.class);
+	}
+	
+	public List<FormaPagamentoModel> toCollectionModel(List<FormaPagamento> formasPagamentos) {
+		return formasPagamentos.stream()
+				.map(formaPagamento -> toModel(formaPagamento))
+				.collect(Collectors.toList());
+	}
+	
+}
